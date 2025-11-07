8b17f7d4db13ee3185b2a0547e1216f0844115f2
cscholer591 <cscholer@gps.deloitte.com>
2025-11-07 12:32:43 -0500

    python 3.13, pillow, django, pandas, numpy upgraded

--- start-app.sh
+++ start-app.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd /tmp/openlxp-xms/app
-python manage.py waitdb 
-python manage.py migrate 
-python manage.py collectstatic --no-input
-python manage.py loaddata admin_theme_data.json 
+python3 manage.py waitdb 
+python3 manage.py migrate 
+python3 manage.py collectstatic --no-input
+python3 manage.py loaddata admin_theme_data.json 
 cd /tmp/
 pwd 
 ./start-server.sh
--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd /tmp/openlxp-xms/app; python manage.py createsuperuser --no-input)
+    (cd /tmp/openlxp-xms/app; python3 manage.py createsuperuser --no-input)
 fi
 (cd /tmp/openlxp-xms/app; gunicorn openlxp_xms_project.wsgi --reload --user 1001 --bind 0.0.0.0:8000 --workers 3)
